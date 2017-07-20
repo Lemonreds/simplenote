ca1c4b5bc8bc18f1fc1ff4adc3f308e1a5b8137c
阿买 <longyuyougu@vip.qq.com>
2017-07-21 00:02:08 +0800

    1.1

---
+++ app/src/main/java/com/example/notes/Activity/UpdateActivity.java
@@ -0,0 +1,19 @@
+package com.example.notes.Activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.ui.R;
+
+public class UpdateActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_update);
+
+
+    }
+
+
+}
